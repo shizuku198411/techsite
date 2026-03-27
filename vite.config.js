768a9d126644a538cf8e0df8c9041fba657d4649
jixeis9541@gmail.com <jixeis9541@gmail.com>
2026-03-27 16:35:00 +0900

    first commit

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 
-const repositoryName = process.env.GITHUB_REPOSITORY?.split("/")[1] ?? "drizzle";
+const repositoryName = process.env.GITHUB_REPOSITORY?.split("/")[1] ?? "techsite";
 const base = process.env.GITHUB_ACTIONS === "true" ? `/${repositoryName}/` : "/";
 
 export default defineConfig({
